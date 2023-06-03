3629f8cdc6d8ff47ee950310f2d04bd9e8a5f052
Adam Steciuk <a.t.steciuk@gmail.com>
2023-06-03 14:29:55 +0200

    Heatmap component refactor

--- src/pages/Heatmap.tsx
+++ src/pages/Heatmap.tsx
@@ -1,27 +1,21 @@
-import 'leaflet/dist/leaflet.css';
-
-import { Icon } from 'leaflet';
-import markerIconPng from 'leaflet/dist/images/marker-icon.png';
 import React, { useEffect, useMemo, useState } from 'react';
-import { MapContainer, Marker, Polygon, Popup, TileLayer } from 'react-leaflet';
-import { VALENBICI_VORONOI } from 'src/apis/valenbici/voronoi/voronoi';
+import LeafletMap from 'src/components/common/map/LeafletMap';
+import Markers from 'src/components/common/map/Markers';
+import Voronoi from 'src/components/common/map/Voronoi';
+import Sidebar from 'src/components/common/Sidebar';
 import useToggle from 'src/hooks/util/useToggle';
 import { UseValenbiciResponse } from 'src/hooks/valenbici/useValenbici';
 
-import CloseIcon from '@mui/icons-material/Close';
-import MenuOpenIcon from '@mui/icons-material/MenuOpen';
-import { Button, FormControlLabel, Slider, Switch } from '@mui/material';
+import { FormControlLabel, Slider, Switch } from '@mui/material';
 
 const Heatmap = (props: { valenbici: UseValenbiciResponse }) => {
-  const center: [number, number] = [39.474809, -0.376574];
   const [stations, _error, loading] = props.valenbici;
 
   const [showStations, toggleShowStations] = useToggle(true);
   const [showVoronoi, toggleShowVoronoi] = useToggle(true);
   const [invertColors, toggleInvertColors] = useToggle(false);
-  const [showSidebar, toggleShowSidebar] = useToggle(true);
 
-  const [availbleRange, setAvailableRange] = useState<[number, number]>([
+  const [availableRange, setAvailableRange] = useState<[number, number]>([
     0, 100,
   ]);
 
@@ -33,23 +27,15 @@ const Heatmap = (props: { valenbici: UseValenbiciResponse }) => {
     return maxTotal;
   }, [stations]);
 
-  const colors = useMemo(() => {
-    const colors: { [key: string]: string } = {};
-    stations.forEach((station) => {
-      if (!station.open) return (colors[station.id] = 'black');
+  const stationsFiltered = useMemo(() => {
+    stations.filter(
+      (station) =>
+        station.available >= availableRange[0] &&
+        station.available <= availableRange[1]
+    );
 
-      const value = station.available / station.total;
-      colors[station.id] = mapValueToColor(invertColors ? 1 - value : value);
-    });
-
-    return colors;
-  }, [stations, invertColors]);
-
-  const stationsFiltered = stations.filter(
-    (station) =>
-      station.available >= availbleRange[0] &&
-      station.available <= availbleRange[1]
-  );
+    return stations;
+  }, [stations, availableRange]);
 
   useEffect(() => {
     setAvailableRange([0, maxTotal]);
@@ -68,120 +54,14 @@ const Heatmap = (props: { valenbici: UseValenbiciResponse }) => {
         overflow: 'hidden',
       }}
     >
-      <MapContainer
-        center={center}
-        zoom={13}
-        scrollWheelZoom={true}
-        css={{
-          flexGrow: 1,
-        }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {showVoronoi &&
-          stationsFiltered
-            .filter((station) => VALENBICI_VORONOI[station.id])
-            .map((station) => (
-              <Polygon
-                key={station.id}
-                positions={VALENBICI_VORONOI[station.id].region}
-                css={{
-                  cursor: 'grab',
-                }}
-                pathOptions={{
-                  stroke: true,
-                  color: 'white',
-                  weight: 2,
-                  opacity: 0.8,
-                  dashArray: '3',
-                  fillOpacity: 0.5,
-                  fillColor: colors[station.id],
-                }}
-              />
-            ))}
-        {showStations &&
-          stationsFiltered.map((station) => (
-            <Marker
-              key={station.id}
-              position={station.position}
-              icon={
-                new Icon({
-                  iconUrl: markerIconPng,
-                  iconSize: [25, 41],
-                  iconAnchor: [12, 41],
-                  popupAnchor: [1, -34],
-                })
-              }
-            >
-              <Popup>
-                <div>{station.address}</div>
-                <div>
-                  Available bikes:{' '}
-                  <b>
-                    {station.available}/{station.total} {'('}
-                    {((station.available / station.total) * 100).toFixed(2)}%
-                    {')'}
-                  </b>
-                </div>
-                {!station.open && (
-                  <div>
-                    <b>Station closed</b>
-                  </div>
-                )}
-              </Popup>
-            </Marker>
-          ))}
-      </MapContainer>
-      <div
-        css={{
-          width: '20rem',
-          position: 'absolute',
-          top: '0',
-          right: showSidebar ? '0' : `-20rem`,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'flex-start',
-          zIndex: 1000,
-          backgroundColor: 'white', // TODO: change to theme
-          padding: '2rem',
-          transition: 'right 0.5s',
-          borderRadius: '0 0 0 0.5rem',
-          '@media (min-width: 1000px)': {
-            right: 0,
-          },
-        }}
-      >
-        <div
-          css={{
-            position: 'absolute',
-            top: '0',
-            left: '-2rem',
-            width: '2rem',
-            height: '2rem',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'white', // TODO: change to theme
-            borderRadius: '0 0 0 0.5rem',
-            '@media (min-width: 1000px)': {
-              display: 'none',
-            },
-          }}
-          onClick={() => toggleShowSidebar()}
-        >
-          {showSidebar ? (
-            <Button>
-              <CloseIcon />
-            </Button>
-          ) : (
-            <Button>
-              <MenuOpenIcon />
-            </Button>
-          )}
-        </div>
+      <LeafletMap>
+        {showVoronoi && (
+          <Voronoi stations={stationsFiltered} invertColors={invertColors} />
+        )}
+        {showStations && <Markers stations={stationsFiltered} />}
+      </LeafletMap>
 
+      <Sidebar>
         <FormControlLabel
           control={
             <Switch
@@ -212,7 +92,7 @@ const Heatmap = (props: { valenbici: UseValenbiciResponse }) => {
         <div className="filtering" css={{ width: '100%', marginTop: '1rem' }}>
           <div>Available bikes range</div>
           <Slider
-            value={availbleRange}
+            value={availableRange}
             onChange={(_event, newValue) => {
               setAvailableRange(newValue as [number, number]);
             }}
@@ -222,14 +102,9 @@ const Heatmap = (props: { valenbici: UseValenbiciResponse }) => {
             step={1}
           />
         </div>
-      </div>
+      </Sidebar>
     </div>
   );
 };
-
-function mapValueToColor(value: number): string {
-  const hue = value * 120;
-  return `hsl(${hue}, 100%, 50%)`;
-}
 
 export default Heatmap;
