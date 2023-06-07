1877a5540f06cd0a16ab89891e297628b1b30450
Adam Steciuk <a.t.steciuk@gmail.com>
2023-06-07 19:49:58 +0200

    Routing and navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from 'src/components/main/Navbar';
-import useValenbici, { UseValenbiciResponse } from 'src/hooks/valenbici/useValenbici';
+import useValenbici, {
+  UseValenbiciResponse,
+} from 'src/hooks/valenbici/useValenbici';
+import Finder from 'src/pages/Finder';
 import Heatmap from 'src/pages/Heatmap';
+import Historic from 'src/pages/Historic';
+import Home from 'src/pages/Home';
 
 export function App() {
   const valenbici: UseValenbiciResponse = useValenbici();
@@ -19,7 +25,12 @@ export function App() {
           flexGrow: 1,
         }}
       >
-        <Heatmap valenbici={valenbici} />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/heatmap" element={<Heatmap valenbici={valenbici} />} />
+          <Route path="/historic" element={<Historic />} />
+          <Route path="/finder" element={<Finder />} />
+        </Routes>
       </main>
     </div>
   );
--- src/components/main/Navbar.tsx
+++ src/components/main/Navbar.tsx
@@ -1,21 +1,136 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import useToggle from 'src/hooks/util/useToggle';
+
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Button } from '@mui/material';
+
+const PATHS: { path: string; name: string }[] = [
+  { path: '/heatmap', name: 'Heatmap' },
+  { path: '/historic', name: 'Historic' },
+  { path: '/finder', name: 'Finder' },
+];
 
 const Navbar = () => {
+  const [navbarOpened, toggleNavbarOpened] = useToggle(false);
+
   return (
-    <nav
+    <div
       css={{
         height: '3rem',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+        display: 'grid',
+        gridTemplateColumns: '8rem 1fr',
         backgroundColor: '#3f51b5',
         color: 'white',
-        fontSize: '1.5rem',
-        fontWeight: 500,
       }}
     >
-      FreeBici
-    </nav>
+      <div
+        css={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'start',
+          fontSize: '1.5rem',
+          fontWeight: 500,
+          paddingLeft: '1rem',
+        }}
+      >
+        <Link to="/">FreeBici</Link>
+      </div>
+
+      <nav
+        css={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'end',
+          paddingRight: '1rem',
+          gap: '1rem',
+          '@media (width < 500px)': {
+            display: 'none',
+          },
+        }}
+      >
+        {PATHS.map((path) => (
+          <Link key={path.path} to={path.path}>
+            <div
+              css={{
+                backgroundColor: '#023047',
+                padding: '0.5rem',
+                borderRadius: '0.2rem',
+                transition: 'background-color 0.2s',
+                '&:hover': {
+                  backgroundColor: '#219ebc',
+                },
+              }}
+            >
+              {path.name}
+            </div>
+          </Link>
+        ))}
+      </nav>
+      <div
+        css={{
+          position: 'relative',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'end',
+          '@media (width >= 500px)': {
+            display: 'none',
+          },
+        }}
+      >
+        <Button onClick={() => toggleNavbarOpened()}>
+          {navbarOpened ? (
+            <CloseIcon
+              css={{
+                color: 'white',
+                fontSize: '2rem',
+              }}
+            />
+          ) : (
+            <MenuIcon
+              css={{
+                color: 'white',
+                fontSize: '2rem',
+              }}
+            />
+          )}
+        </Button>
+        {navbarOpened && (
+          <nav
+            css={{
+              position: 'absolute',
+              top: '3rem',
+              right: 0,
+              backgroundColor: '#3f51b5',
+              padding: '0.5rem',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '0.5rem',
+              zIndex: 1001,
+            }}
+          >
+            {PATHS.map((path) => (
+              <Link key={path.path} to={path.path}>
+                <div
+                  css={{
+                    backgroundColor: '#023047',
+                    padding: '0.5rem',
+                    borderRadius: '0.2rem',
+                    transition: 'background-color 0.2s',
+                    '&:hover': {
+                      backgroundColor: '#219ebc',
+                    },
+                  }}
+                >
+                  {path.name}
+                </div>
+              </Link>
+            ))}
+          </nav>
+        )}
+      </div>
+    </div>
   );
 };
 
