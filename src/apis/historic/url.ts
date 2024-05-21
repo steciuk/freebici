e15d808bb0b31e76e0a3ff0f237a03e7ddd7998c
steciuk <a.t.steciuk@gmail.com>
2024-05-21 13:34:48 +0200

    Move static data to assets

--- src/apis/historic/url.ts
+++ src/apis/historic/url.ts
@@ -1,4 +1,11 @@
+const host = window.location.host;
+const isDev = process.env.NODE_ENV === 'development';
+
 export const STATIC_URL =
-  'https://steciuk.github.io/freebici/valenbici_static.json';
+  host + isDev
+    ? '/src/assets/data/valenbici_static.json'
+    : '/assets/data/valenbici_static.json';
 export const HISTORIC_URL =
-  'https://steciuk.github.io/freebici/valenbici_historic.json';
+  host + isDev
+    ? '/src/assets/data/valenbici_historic.json'
+    : '/assets/data/valenbici_historic.json';
